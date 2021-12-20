333dbabbc2c9fb55ad0d67fec454bdd0c6fd4d78
“gil-son” <gilszon@gmail.com>
2021-12-20 20:13:55 -0300

    Camel Rest API

---
+++ camel-rest-dsl/src/main/java/com/javatechie/spring/camel/api/resource/ApplicationResource.java
@@ -0,0 +1,28 @@
+package com.javatechie.spring.camel.api.resource;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationResource extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+
+		restConfiguration()
+			.component("servlet")
+			.port(9090)
+			.host("localhost")
+			.bindingMode(RestBindingMode.json);
+		
+		rest()
+		.get("/hello-world")
+		.produces(MediaType.APPLICATION_JSON_VALUE)
+		.route()
+		.setBody(constant("Welcome to java techie"));
+		
+	}
+
+}
